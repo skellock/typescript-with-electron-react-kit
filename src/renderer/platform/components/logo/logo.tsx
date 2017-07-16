3eb280d7a0aa36c986f67bb836afc0cd3f7f3508
Steve Kellock <steve@kellock.ca>
2017-07-16 14:00:37 -0400

    Loosly packs image assets instead.

--- src/renderer/platform/components/logo/logo.tsx
+++ src/renderer/platform/components/logo/logo.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
-// Just until this ships:
-// https://github.com/fuse-box/fuse-box/pull/672
-// 
-// then we can drop the '* as'
-import * as icon from './electron-icon.svg'
+import icon from './electron-icon.svg'
 
 const style = {
   width: 80,
