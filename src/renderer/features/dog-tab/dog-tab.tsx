7e8c6a04c71dd290f08b7c49280c85f10e799094
Steve Kellock <steve@kellock.ca>
2017-07-19 19:19:27 -0400

    Removes the tooltip.

--- src/renderer/features/dog-tab/dog-tab.tsx
+++ src/renderer/features/dog-tab/dog-tab.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react'
 import { Text, FunDog, Logo, CenteredContent } from '../../platform'
-import { Tooltip } from 'rebass'
 
 export class DogTab extends React.Component<{}, {}> {
   render() {
     return (
       <CenteredContent>
         <FunDog />
-        <Text py={3}>
-          Wake up and smell the{' '}
-          <Tooltip text="😖 sad.">
-            <Text color='teal5' bold>electrons</Text>
-          </Tooltip>.
-        </Text>
+        <Text py={3}>Wake up and smell the electrons.</Text>
         <Logo />
       </CenteredContent>
     )
