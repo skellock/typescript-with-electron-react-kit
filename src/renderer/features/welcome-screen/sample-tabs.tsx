58e944af7e1b8d995654f6c8e2de347c4c97ff73
Steve Kellock <steve@kellock.ca>
2017-07-19 17:40:54 -0400

    Adds keyboard support to the renderer.

--- src/renderer/features/welcome-screen/sample-tabs.tsx
+++ src/renderer/features/welcome-screen/sample-tabs.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import { Tabs, TabItem } from 'rebass'
+import { bindKey, unbindKey } from '../../platform'
 
 export type SampleTabType = 'one' | 'two' | 'three'
+
 export interface SampleTabsProps {
   tab: SampleTabType
   onChangeTab: (tab: SampleTabType) => void
@@ -10,30 +12,39 @@ export interface SampleTabsProps {
 const style = { WebkitAppRegion: 'drag' }
 const tabStyle = { cursor: 'pointer', WebkitAppRegion: 'no-drag' }
 
-export function SampleTabs(props: SampleTabsProps) {
-  return (
-    <Tabs px={3} bg="gray1" style={style}>
-      <TabItem
-        style={tabStyle}
-        onClick={() => props.onChangeTab('one')}
-        active={props.tab === 'one'}
-      >
-        Random Dog
-      </TabItem>
-      <TabItem
-        style={tabStyle}
-        onClick={() => props.onChangeTab('two')}
-        active={props.tab === 'two'}
-      >
-        Long Content
-      </TabItem>
-      <TabItem
-        style={tabStyle}
-        onClick={() => props.onChangeTab('three')}
-        active={props.tab === 'three'}
-      >
-        Empty
-      </TabItem>
-    </Tabs>
-  )
+export class SampleTabs extends React.PureComponent<SampleTabsProps, {}> {
+  tab1 = () => this.props.onChangeTab('one')
+  tab2 = () => this.props.onChangeTab('two')
+  tab3 = () => this.props.onChangeTab('three')
+
+  componentDidMount() {
+    bindKey('command+1', () => this.tab1())
+    bindKey('command+2', () => this.tab2())
+    bindKey('command+3', () => this.tab3())
+  }
+
+  componentWillUnmount() {
+    unbindKey('command+1')
+    unbindKey('command+2')
+    unbindKey('command+3')
+  }
+
+  render() {
+    const isTab1 = this.props.tab === 'one'
+    const isTab2 = this.props.tab === 'two'
+    const isTab3 = this.props.tab === 'three'
+    return (
+      <Tabs px={3} bg="gray1" style={style}>
+        <TabItem style={tabStyle} onClick={() => this.tab1()} active={isTab1}>
+          Random Dog
+        </TabItem>
+        <TabItem style={tabStyle} onClick={() => this.tab2()} active={isTab2}>
+          Long Content
+        </TabItem>
+        <TabItem style={tabStyle} onClick={() => this.tab3()} active={isTab3}>
+          Empty
+        </TabItem>
+      </Tabs>
+    )
+  }
 }
--- src/renderer/platform/index.ts
+++ src/renderer/platform/index.ts
@@ -1,2 +1,3 @@
+export * from './utils'
 export * from './theme/theme'
 export * from './components'
