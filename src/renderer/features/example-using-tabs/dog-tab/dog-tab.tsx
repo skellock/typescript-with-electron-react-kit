58f5e71de0cdff1ab8d707751b8ba9495d4c65df
Steve Kellock <steve@kellock.ca>
2017-08-06 20:55:00 -0400

    Updates some random styles.

--- src/renderer/features/example-using-tabs/dog-tab/dog-tab.tsx
+++ src/renderer/features/example-using-tabs/dog-tab/dog-tab.tsx
@@ -4,10 +4,13 @@ import { Text, CenteredContent, spacing, cssProps } from '../../../platform'
 import { FunDog } from '../fun-dog'
 import { Logo } from '../logo'
 
-const TEXT_STYLE = cssProps({
-  paddingTop: spacing.medium,
-  paddingBottom: spacing.medium,
+const TEXT_STYLE = cssProps({})
+
+const DOG_STYLE = cssProps({
+  marginTop: spacing.medium,
+  marginBottom: spacing.medium,
 })
+
 export interface DogTabProps {
   style?: CSSProperties | CSSProperties[] | null | false
 }
@@ -17,7 +20,7 @@ export class DogTab extends React.PureComponent<DogTabProps, {}> {
     return (
       <CenteredContent style={this.props.style}>
         <Text style={TEXT_STYLE}>Do a barrel roll.</Text>
-        <FunDog />
+        <FunDog style={DOG_STYLE} />
         <Logo />
       </CenteredContent>
     )
