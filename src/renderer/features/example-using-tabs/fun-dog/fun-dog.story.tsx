3c4f08a8dc7b88d16e705f08a4d2058814ba7101
Richard Evans <rmevans9@gmail.com>
2017-08-13 12:51:11 -0400

    Get the last of it working.

---
+++ src/renderer/features/example-using-tabs/fun-dog/fun-dog.story.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { FunDog } from './index'
+
+storiesOf('Fun Dog', module).add('default', () => <FunDog />)
--- src/renderer/platform/components/text/text.story.tsx
+++ src/renderer/platform/components/text/text.story.tsx
@@ -3,4 +3,8 @@ import { storiesOf } from '@storybook/react'
 
 import { Text } from './index'
 
-storiesOf('Text', module).add('default', () => <Text>Hello World!</Text>)
+storiesOf('Text', module).add('default', () => <Text>Hello World!</Text>).add('with children', () =>
+  <Text>
+    <strong>I am STRONG</strong>
+  </Text>,
+)
