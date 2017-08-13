b9c103842a423afa3fa89903e76b45e712364c30
Richard Evans <rmevans9@gmail.com>
2017-08-13 12:25:52 -0400

    Start of adding storybook

---
+++ src/renderer/platform/components/text/text.story.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { Text } from './index'
+
+storiesOf('Text', module).add('default', () => <Text>Hello World!</Text>)
